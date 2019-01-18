f97fd55b4ef3a9f801c78f451517034a1fa522b0
Tim7310 <yukinoshita731@gmail.com>
2019-01-19 07:30:28 +0800

    Receipt Updated

--- cashier/Receipt.php
+++ cashier/Receipt.php
@@ -1,9 +1,9 @@
-<script type="text/javascript">
+﻿<!-- <script type="text/javascript">
 	window.onload = function() 
 	{ 
 		window.print(); 
 	}
-</script>
+</script> -->
 <?php
 include_once('../connection.php');
 include_once('../classes/patient.php');
@@ -30,7 +30,7 @@ if (isset($_GET['transID'])){
 	body
 		{
 			font-family: "Century Gothic";
-			font-size: 16px;
+			font-size: 14px;
 			padding: 0px;
 			margin: 0px;
 		}
@@ -71,12 +71,14 @@ if (isset($_GET['transID'])){
 	</style>
 <body>
 <br><br>
-<table style="margin-left:10px;">
+<table style="margin-left:20px;margin: right:20px;width: 200px;margin-top:-20px">
 	<tr>
-		<td colspan="2"><?php echo $data['TransactionDate'] ?></td>
+		<td colspan="3"><?php echo $data['TransactionDate'] ?></td>
+		<td rowspan="23" style="width: 20px">&nbsp;&nbsp;&nbsp;</td>
 	</tr>
 	<tr>
 		<td colspan="3" style=""><center><b>QUEST PHIL DIAGNOSTICS</b></center></td>
+
 	</tr>
 	<tr>
 		<td colspan="3" style=""><center>McArthur Hi-Way Cor. Salome Rd. Angeles City</center></td>
@@ -84,25 +86,25 @@ if (isset($_GET['transID'])){
 	<tr style="height: 25px"></tr>
 	<tr >
 		<td class="text-right" >Name:</td>
-		<td class="" style="font-size: 19px;"><b>
+		<td class="" style="font-size: 14px;" colspan="2"><b>
 			&nbsp;&nbsp;<?php echo $data['LastName'] ?>, <?php echo $data['FirstName'] ?> <?php echo $data['MiddleName'] ?>
 		</b></td>
 	</tr>
 	<tr>
 		<td class="text-right">Company:</td>
-		<td class=""><b>
+		<td class="" colspan="2" style="font-size: 14px;"><b>
 			&nbsp;&nbsp;<?php echo $data['CompanyName'] ?>
 		</b></td>
 	</tr>
 	<tr>
 		<td class="text-right">Age/Gen:</td>
-		<td class=""><b>
+		<td class="" style="font-size: 14px;"><b>
 			&nbsp;&nbsp;<?php echo $data['Age'] ?>/<?php echo $data['Gender'] ?>
 		</b></td>
 	</tr>
 	<tr>
 		<td class="text-right">DOB:</td>
-		<td class=""><b>
+		<td class="" style="font-size: 14px;"><b>
 			&nbsp;&nbsp;<?php echo $data['Birthdate'] ?>
 		</b></td>
 	</tr>
@@ -114,14 +116,14 @@ if (isset($_GET['transID'])){
 			$reff = $data['Biller'];
 		}
 		?>
-		<td class="text-right">Referral:</td>
-		<td class=""><b>
+		<td class="text-right" >Referral:</td>
+		<td class="" colspan="2" style="font-size: 14px;"><b>
 			&nbsp;&nbsp;<?php echo $reff ?>
 		</b></td>
 	</tr>
 	<tr>
 		<td class="text-right">Cashier:</td>
-		<td class=""><b>
+		<td class="" style="font-size: 14px;"><b>
 			&nbsp;&nbsp;<?php 
 				$cashier = explode(",",$data['Cashier']);
 				echo $cashier[0];
@@ -130,27 +132,29 @@ if (isset($_GET['transID'])){
 	</tr>
 	<tr>
 		<td class="text-right">Type:</td>
-		<td class=""><b>
+		<td class="" style="font-size: 14px;"><b>
 			&nbsp;&nbsp;<?php echo $data['TransactionType'] ?>
 		</b></td>
 	</tr>
 	<tr>
 		<td class="text-right"><h4>RCPT:</h4></td>
-		<td class=""><b>
+		<td class="" colspan="2"><b>
 			<h4><b>&nbsp;#<?php echo $data['TransactionID'] ?></b></h4>
 		</b></td>
 	</tr>
 	<tr class="tab" >
-		<td style="width: 100px;padding: 5px;"><b>Items</b></td>
-		<td style="width: 200px;padding: 5px;"><b>Description</b></td>
-		<td style="width: 80px;padding: 5px;" class="text-right"><b>Price</b></td>
+		
+		<td style="padding: 5px;"><b>Items</b></td>
+		<td></td>
+	<!-- 	<td style="width: 200px;padding: 5px;"><b>Description</b></td> -->
+		<td style="width: 120px;padding: 5px;" class="text-right"><b>Price</b></td>
 	</tr>
 		<?php
 			$ItemName = explode(",",$data['ItemID']);
 			foreach ($ItemName as $item) {
 		?>
 	<tr style="border-bottom: dotted grey 1px; ">
-		<td style="word-wrap: break-word;width: 100px;">
+		<td style="word-wrap: break-word;width: 150px;" colspan="2">
 			<b>
 				<?php 
 					$itemdata = $trans->fetch_item($item);
@@ -159,11 +163,11 @@ if (isset($_GET['transID'])){
 				?>
 			</b>
 		</td>
-		<td style="word-wrap: break-word;padding-left:10px;padding-right: 10px;width: 200px;">
+		<!-- <td style="word-wrap: break-word;padding-left:10px;padding-right: 10px;width: 200px;">
 			<?php 
 				echo $itemdata['ItemDescription'];
 			?>
-		</td>
+		</td> -->
 		<td style="" class="text-right">
 			<?php 
 				echo $itemdata['ItemPrice']."<br/>";
@@ -173,24 +177,22 @@ if (isset($_GET['transID'])){
 	<?php } ?> 
 		<tr class="tab" ></tr>
 				<tr style="border-top: solid grey 5px; padding-top: 3px">
-					<td style="width: 100px;" ></td>
-					<td style="width: 200px;" class="text-right">Receipt Total:</td>
-					<td style="width: 80px;" class="text-right">₱:<?php echo $price_total ?></td>
+					<td style="" class="text-right" colspan="2">Receipt Total:</td>
+					<td style="" class="text-right">₱:<?php echo $price_total ?></td>
 				</tr>
 				<?php
 					if ($data['TransactionType'] == 'ACCOUNT') {
 						
 				?>
 				<tr>
-					<td style="width: 100px"></td>
-					<td style="width: 200px;font-size: 18px; font-weight: bolder;" class="text-right">Total Accounted:</td>
-					<td style="width: 80px;font-size: 18px; font-weight: bolder;" class="text-right">
+					<td style="font-size: 18px; font-weight: bolder;" colspan="2" 
+					class="text-right">Total Accounted:</td>
+					<td style="font-size: 18px; font-weight: bolder;" class="text-right">
 						₱:<?php echo $data['GrandTotal'] ?></td>
 				</tr>				
 				<tr>
-					<td style="width: 100px"></td>
-					<td style="width: 200px;font-size: 18px; font-weight: bolder;" class="text-right">Billed to:</td>
-					<td style="width: 80px;font-size: 18px; font-weight: bolder;" class="text-right">
+					<td style="font-size: 18px; font-weight: bolder;" colspan="2" class="text-right">Billed to:</td>
+					<td style="font-size: 18px; font-weight: bolder;" class="text-right">
 						<?php echo $data['Biller'] ?></td>
 				</tr>
 					<?php } ?>
@@ -200,9 +202,8 @@ if (isset($_GET['transID'])){
 						
 				?>
 				<tr>
-					<td style="width: 100px"></td>
-					<td style="width: 200px" class="text-right">Discount:</td>
-					<td style="width: 80px" class="text-right">₱:
+					<td style="" class="text-right" colspan="2">Discount:</td>
+					<td style="" class="text-right">₱:
 					<?php 
 						// $Disc = explode(",",$data['Discount']);
 						// $DiscA = array_sum($Disc);
@@ -211,15 +212,14 @@ if (isset($_GET['transID'])){
 					?></td>
 				</tr>
 				<tr>
-					<td style="width: 100px"></td>
-					<td style="width: 200px;font-size: 18px; font-weight: bolder;" class="text-right">Total Accounted:</td>
-					<td style="width: 80px;font-size: 18px; font-weight: bolder;" class="text-right">
+					<td style="font-size: 18px; font-weight: bolder;" class="text-right" colspan="2">
+					Total Accounted:</td>
+					<td style="font-size: 18px; font-weight: bolder;" class="text-right">
 						₱:<?php echo $data['GrandTotal'] ?></td>
 				</tr>	
 				<tr>
-					<td style="width: 100px"></td>
-					<td style="width: 200px" class="text-right">Amount Tendered:</td>
-					<td style="width: 80px" class="text-right">₱:
+					<td style="" class="text-right" colspan="2">Amount Tendered:</td>
+					<td style="" class="text-right">₱:
 					<?php 
 						$cash = explode(",",$data['PaidIn']);
 						echo $cash[0];
@@ -228,10 +228,9 @@ if (isset($_GET['transID'])){
 				</tr>
 				
 				<tr>
-					<td style="width: 100px"></td>
-					<td style="width: 200px;font-size: 18px; font-weight: bolder;" class="text-right">
+					<td style="font-size: 18px; font-weight: bolder;" class="text-right" colspan="2">
 					Given Change:</td>					
-					<td style="width: 80px;font-size: 18px; font-weight: bolder;" class="text-right">₱:
+					<td style="font-size: 18px; font-weight: bolder;" class="text-right">₱:
 					<?php 
 						$change = explode(",",$data['PaidOut']);
 						echo $change[0];
@@ -242,19 +241,15 @@ if (isset($_GET['transID'])){
 					<td style="height: 30px;"></td>
 				</tr>
 				<tr>
-					<td ></td>
-					<td style="padding-top: 15px;">
-						<center><p style="border: solid 2px black;padding-left: 25px;padding-right: 25px;font-size: 17px">
+					<td style="padding-top: 15px;" colspan="3">
+						<center><p style="border: solid 2px black;padding-left: 25px;padding-right: 25px;font-size: 17px;width: 250px">
 						online results,<br/> www.questphil.com.ph</p></center>		
 					</td>
-					<td></td>
 				</tr>
 				<tr>
-					<td></td>
-					<td><center>
+					<td colspan="3"><center>
 						<img src="bar.jpg" height="30px" width="250px" style="margin: 0px; padding: 0px;">
 					</center></td>
-					<td></td>
 				</tr>
 </table>	
 
